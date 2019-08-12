379fa765fe13de4a8b7f9c28b87622613f5133dd
agatharach <agatha@alterra.id>
2019-08-12 14:07:43 +0700

    edit sedikit bagian import

--- src/About.js
+++ src/About.js
@@ -1,36 +1,16 @@
-import React from 'react';
-import logo from './assets-final-project-fe/img/logo-ALTA.png';
-import dot from './assets-final-project-fe/img/img-dot.png';
-import homeimg from './assets-final-project-fe/img/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg';
-import logosmall from './assets-final-project-fe/img/logo-ALTA-v2.png';
-import fb from './assets-final-project-fe/img/ic_fb.png';
-import twitter from './assets-final-project-fe/img/ic-twitter.png';
-import ig from './assets-final-project-fe/img/ic-instagram.png';
-import LinkedIn from './assets-final-project-fe/img/ic-linkedin.png';
-import './assets-final-project-fe/CSS/less/style.css';
+import React from "react";
+import dot from "./assets-final-project-fe/img/img-dot.png";
+import homeimg from "./assets-final-project-fe/img/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg";
+import logosmall from "./assets-final-project-fe/img/logo-ALTA-v2.png";
+import fb from "./assets-final-project-fe/img/ic_fb.png";
+import twitter from "./assets-final-project-fe/img/ic-twitter.png";
+import ig from "./assets-final-project-fe/img/ic-instagram.png";
+import LinkedIn from "./assets-final-project-fe/img/ic-linkedin.png";
+import "./assets-final-project-fe/CSS/less/style.css";
 
-function About (){
-    return(
+function About() {
+    return (
         <div>
-            <header>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-3 col-12 ">
-                            <div className="logo">
-                                <a><img src={logo} alt="Logo" /></a>
-                            </div>
-                        </div>
-                        <div className="col-md-9">
-                            <ul className="menu">
-                                <li><a href="home.html" id="home">home</a></li>
-                                <li><a href="about.html">about</a></li>
-                                <li><a>experience</a></li>
-                                <li><a href="contact.html">contact</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </header>
             <div className="wrapper-about">
                 <section className="about-me">
                     <div className="container-fluid">
@@ -45,7 +25,22 @@ function About (){
                     <div className="container">
                         <div className="row">
                             <div className="col-md-7">
-                                <p>Hello! I’m Anne Sullivan, a full-stack engineer based in Malang, IDN who enjoys building things that live on the internet. I develop exceptional websites and web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends. <br/><br/> Shortly after graduating from Alterra Academy, I joined the engineering team at Alterra where I work on a wide variety of interesting and meaningful projects on a daily basis. <br/><br/> Here’s few technologies I’ve been working with recently :</p>
+                                <p>
+                                    Hello! I’m Anne Sullivan, a full-stack
+                                    engineer based in Malang, IDN who enjoys
+                                    building things that live on the internet. I
+                                    develop exceptional websites and web apps
+                                    that provide intuitive, pixel-perfect user
+                                    interfaces with efficient and modern
+                                    backends. <br />
+                                    <br /> Shortly after graduating from Alterra
+                                    Academy, I joined the engineering team at
+                                    Alterra where I work on a wide variety of
+                                    interesting and meaningful projects on a
+                                    daily basis. <br />
+                                    <br /> Here’s few technologies I’ve been
+                                    working with recently :
+                                </p>
                                 <table>
                                     <tr>
                                         <td>HTML & CSS</td>
@@ -60,7 +55,9 @@ function About (){
                                     <tr>
                                         <td>Database</td>
                                         <td className="td2">Javascript</td>
-                                        <td className="td3">Software Testing</td>
+                                        <td className="td3">
+                                            Software Testing
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td>Git & Github</td>
@@ -71,38 +68,44 @@ function About (){
                             </div>
                             <div className="col-md-5 align-items-center">
                                 <div className="image-about">
-                                <img src={homeimg} alt="" className="about-me-img"/>
-                                <img src={dot} alt="" className="dot"/>
+                                    <img
+                                        src={homeimg}
+                                        alt=""
+                                        className="about-me-img"
+                                    />
+                                    <img src={dot} alt="" className="dot" />
                                 </div>
                             </div>
                         </div>
                     </div>
-                </section>  
+                </section>
             </div>
             <footer className="container-fluid">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-5 col-12 aligness">
-                            <img src={logosmall} width="110 px" alt="logosmall"/>
+                            <img
+                                src={logosmall}
+                                width="110 px"
+                                alt="logosmall"
+                            />
                         </div>
                         <div className="col-md-4 col-12 ">
-                            
-                                <div className="SocialMedia">Social Media</div>
-                                <div className="sosmed">
+                            <div className="SocialMedia">Social Media</div>
+                            <div className="sosmed">
                                 <div className="facebook aligness">
-                                    <img src={fb} alt="fb"/>
+                                    <img src={fb} alt="fb" />
                                 </div>
                                 <div className="Twitter aligness">
-                                    <img src={twitter} alt="twit"/>
+                                    <img src={twitter} alt="twit" />
                                 </div>
                                 <div className="Instagram aligness">
-                                    <img src={ig} alt="ig"/>
+                                    <img src={ig} alt="ig" />
                                 </div>
                                 <div className="LinkedIn aligness">
-                                    <img src={LinkedIn} alt="linkedin"/>
+                                    <img src={LinkedIn} alt="linkedin" />
                                 </div>
-                                </div>
-                    
+                            </div>
                         </div>
                         <div className="col-md-3 col-12">
                             <h5>copyright &copy; 2019 Alterra</h5>
@@ -111,7 +114,7 @@ function About (){
                 </div>
             </footer>
         </div>
-    )
+    );
 }
 
 export default About;
--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './assets-final-project-fe/img/logo-ALTA.png';
-import homeimg from './assets-final-project-fe/img/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg';
+import React from "react";
+import logo from "./assets-final-project-fe/img/logo-ALTA.png";
+import homeimg from "./assets-final-project-fe/img/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg";
+import "./assets-final-project-fe/CSS/less/style.css";
 
-function Home (){
-    return(
+function Home() {
+    return (
         <body className="home">
             <header>
                 <div className="container">
                     <div className="row">
                         <div className="col-md-3 col-12 ">
                             <div className="logo">
-                                    <a><img src={logo} alt="Logo" /></a>
+                                <a>
+                                    <img src={logo} alt="Logo" />
+                                </a>
                             </div>
                         </div>
                         <div className="col-md-9">
                             <ul className="menu">
-                                <li><a href="home.html" id="home">home</a></li>
-                                <li><a href="about.html">about</a></li>
-                                <li><a>experience</a></li>
-                                <li><a href="contact.html">contact</a></li>
+                                <li>
+                                    <a href="home.html" id="home">
+                                        home
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="about.html">about</a>
+                                </li>
+                                <li>
+                                    <a>experience</a>
+                                </li>
+                                <li>
+                                    <a href="contact.html">contact</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -29,28 +42,28 @@ function Home (){
                     <div className="row align-items-center contain">
                         <div className="col-md-5  col-12 content">
                             <div className="logo">
-                                <img src={homeimg}  alt="home" className="home-image"/>
+                                <img
+                                    src={homeimg}
+                                    alt="home"
+                                    className="home-image"
+                                />
                             </div>
                         </div>
                         <div className="col-md-7 col-12">
-                            <div className="name1">
-                                Hi, my name is
-                            </div>
-                            <div className="name2">
-                                Anne Sullivan
-                            </div>
+                            <div className="name1">Hi, my name is</div>
+                            <div className="name2">Anne Sullivan</div>
                             <div className="name3">
                                 I build things for the web
                             </div>
-                            <a className="name4" href="contact.html" >
+                            <a className="name4" href="contact.html">
                                 Get in Touch
                             </a>
-                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </body>
-    )
+    );
 }
 
 export default Home;
